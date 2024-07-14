f5b5d736159b185cd5323f7c1592b540df5ea2c5
SharvariMore <sharvarimore90@gmail.com>
2024-07-14 07:36:09 -0400

    Added public url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env("PUBLIC_URL", "https://strapi-grocery-shopp.onrender.com"),
+  url: env("PUBLIC_URL", "https://strapi-grocery-shop.onrender.com"),
   app: {
     keys: env.array('APP_KEYS'),
   },
